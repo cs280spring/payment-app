e7367a1a25d64d926cfa4feba139df36330371c8
Ali Madooei <alimadooei@gmail.com>
2021-09-06 10:12:24 -0400

    Step 5 Validate date

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ submitBtn.addEventListener("click", handleFormSubmit);
 
 function handleFormSubmit(event) {
   event.preventDefault();
+
+  const month = document.getElementById("month").value;
+  const year = document.getElementById("year").value;
+  if (new Date() > new Date(year, month)) {
+    window.alert("Your card is expired!");
+    return;
+  }
+  
   window.alert("Thanks for the payment!");
 }
