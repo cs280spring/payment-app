384d34a133b18b7c723cb8d4d25b1e098c949360
Ali Madooei <alimadooei@gmail.com>
2021-09-06 10:28:08 -0400

    Step 6 Validate CVV

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ function handleFormSubmit(event) {
     window.alert("Your card is expired!");
     return;
   }
-  
+
+  const cvv = document.getElementById("cvv").value;
+  if (!/^[0-9]{3,4}$/.test(cvv)) {
+    window.alert("Invalid CVV. It must be 3 or 4 digits!");
+    return;
+  }
+
   window.alert("Thanks for the payment!");
 }
